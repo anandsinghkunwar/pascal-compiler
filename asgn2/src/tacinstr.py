b40296e03385b4536b13bf7b6e62623dfa8a81ae
Saransh Srivastava <ssaransh@cse.iitk.ac.in>
2016-02-01 00:00:54 +0530

    Operation and instruction types

--- asgn2/src/tacinstr.py
+++ asgn2/src/tacinstr.py
@@ -1,13 +1,41 @@
+# Class to define a symbol table entry.
+class SymTabEntry(object):
+    def __init__(self):
+        pass
+
 # Class to define a Three Address Code Instruction (TACInstr).
 
 class TACInstr(object):
     def __init__(self):
         self.InstrType = None
         self.Target = None
+        self.Op = None
+        self.Src1 = SymTabEntry()
+        self.Src2 = SymTabEntry()
+        self.Dest = SymTabEntry()
+        self.SymTable = None
 
+    # Types of operations
+    ADD, SUB, MULT, DIV, GT, LT, GEQ, LEQ, NEQ, SHL, SHR, AND, NOT, OR, MOD, XOR = range(16)
+
+    # Operation map
+    OpMap = {
+                "+"     : ADD,      "-"     : SUB,      "*"     : MULT,
+                "/"     : DIV,      ">"     : GT,       "<"     : LT,
+                ">=",   : GEQ,      "<="    : LEQ,      "<>"    : NEQ,
+                "<<",   : SHL,      ">>"    : SHR,      "and"   : AND,
+                "not"   : NOT,      "or"    : OR,       "mod"   : MOD,
+                "xor"   : XOR
+            }
 
     # Types of instructions
-    ASSIGN, IFGOTO, GOTO, CALL, RETURN, NONE = xrange(6)
+    ASSIGN, IFGOTO, GOTO, CALL, RETURN = range(6)
+
+    # Instruction map
+    InstrMap = {
+                "="     : ASSIGN,      "ifgoto"     : IFGOTO,      "goto"     : GOTO,
+                "call"  : CALL,        "ret"        : RETURN
+               }
 
     # Methods to check type of the instruction 
     def isAssign(self):
