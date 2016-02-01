b75343365f672c13e97fdde1e95c72f7167e04f7
Saransh Srivastava <ssaransh@cse.iitk.ac.in>
2016-02-02 00:20:20 +0530

    Restructured machine module. Some methods incomplete

--- asgn2/src/machine.py
+++ asgn2/src/machine.py
@@ -1,18 +1,28 @@
-# Class to implement machine registers.
+# Contains a model of the machine assembly code
+#   - a class to implement machine registers
+#   - a class to implement the (global) data region
+#   - a class to implement the code section
 
 import tacinstr, basicblock
 
 # Global variable to store number of general purpose registers
 numRegs = 6
 
+# Global list of register names
+registers = ['eax', 'ebx', 'ecx', 'edx', 'edi', 'esi']
+
+# Class to implement machine registers.
 class Register(object):
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.value = 0
         self.empty = True
-        self.
     
     def value(self):
         return self.value
 
     def isEmpty(self):
         return self.empty
+
+    def spill(self):
+        pass
