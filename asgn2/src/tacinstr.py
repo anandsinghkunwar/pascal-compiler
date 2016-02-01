13e4a5e1184d40f080adabe97b9c4a13a049787a
Saransh Srivastava <ssaransh@cse.iitk.ac.in>
2016-02-01 18:25:37 +0530

    Added basic structure for parsing the instruction tuples in the IR

--- asgn2/src/tacinstr.py
+++ asgn2/src/tacinstr.py
@@ -6,7 +6,7 @@ class SymTabEntry(object):
 # Class to define a Three Address Code Instruction (TACInstr).
 
 class TACInstr(object):
-    def __init__(self):
+    def __init__(self, instrTuple):
         self.InstrType = None
         self.Target = None
         self.Op = None
@@ -14,6 +14,45 @@ class TACInstr(object):
         self.Src2 = SymTabEntry()
         self.Dest = SymTabEntry()
         self.SymTable = None
+
+        # Process the instrTuple to populate the member fields
+        self.InstrType = TACInstr.InstrMap[instrTuple[1]]
+        # Now, the parsing diverges for each type
+        if self.InstrType == TACInstr.ASSIGN:
+            if   len(instrTuple) == 4:   # Basic assignment: 1, =, a, b
+                pass
+            elif len(instrTuple) == 5:   # Assignment with unary op: 2, =, -, g, f
+                pass
+            elif len(instrTuple) == 6:   # Assignment with binary op: 3, =, +, a, b, c
+                pass
+            else:
+                # Error
+                pass
+        elif self.InstrType == TACInstr.IFGOTO:
+            if len(instrTuple) == 6:    # Tuple: 4, ifgoto, relop, i, j, L
+                self.Target = int(instrTuple[5])
+                pass
+            else:
+                # Error
+                pass
+        elif self.InstrType == TACInstr.GOTO:
+            if len(instrTuple) == 3:    # Tuple: 5, goto, L1
+                self.Target = int(instrTuple[2])
+            else:
+                # Error
+                pass
+        elif self.InstrType == TACInstr.CALL:
+            if len(instrTuple) == 3:    # Tuple: 6, call, foo
+                pass
+            else:
+                # Error
+                pass
+        elif self.InstrType == TACInstr.RETURN:
+            if len(instrTuple) == 3:    # Tuple: 7, ret, retval
+                pass
+            else:
+                # Error
+                pass
 
     # Types of operations
     ADD, SUB, MULT, DIV, GT, LT, GEQ, LEQ, NEQ, SHL, SHR, AND, NOT, OR, MOD, XOR = range(16)
@@ -29,7 +68,7 @@ class TACInstr(object):
             }
 
     # Types of instructions
-    ASSIGN, IFGOTO, GOTO, CALL, RETURN = range(6)
+    ASSIGN, IFGOTO, GOTO, CALL, RETURN = range(5)
 
     # Instruction map
     InstrMap = {
