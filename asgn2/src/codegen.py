01987c34592e850361ce3506264987d8d83683a9
Saransh Srivastava <ssaransh@cse.iitk.ac.in>
2016-02-03 23:41:42 +0530

    Completely incomplete codegen.py. TODO: Compute basic blocks

--- asgn2/src/codegen.py
+++ asgn2/src/codegen.py
@@ -2,3 +2,12 @@
 # symbol table, register descriptor, address descriptor.
 
 import tacinstr, basicblock, machine
+
+# Class to define the code generator
+class Codegen(object):
+    def __init__(self, program):
+        self.program = program
+        self.basicBlocks = []
+    # Method to compute basic blocks from the IR program
+    def getBasicBlocks(self):
+        for instr in program:
