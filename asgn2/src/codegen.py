31a721941405bcf914bd331a0038a3743c278c4b
Saransh Srivastava <ssaransh@cse.iitk.ac.in>
2016-02-05 02:21:45 +0530

    Added pairwise helper function for more readable basic block computation code

--- asgn2/src/codegen.py
+++ asgn2/src/codegen.py
@@ -1,27 +1,26 @@
 # Main code generator module. Contains description of the
 # symbol table, register descriptor, address descriptor.
-
 import tacinstr, basicblock, machine
+from itertools import tee, izip
 
 # Class to define the code generator
 class Codegen(object):
     def __init__(self, program):
         self.program = program
         self.basicBlocks = []
-    # Method to compute basic blocks from the IR program
-    def getBasicBlocks(self):
-        leaders = set()
 
+    # Method to compute basic blocks from the IR program
+    def computeBasicBlocks(self):
+        leaders = set([1, len(self.program)])
         for instr in self.program:      #Yet to add for calling/return statements
             if instr.isIfGoto() or instr.isGoto():
                 leaders.add(instr.Target)
                 leaders.add(instr.LineNo + 1)
-        basicBlocks = []
-        prev = 0
-        for l in leaders:
-            basicBlocks.append(self.prog[prev:l-1])   #all this assuming program 
-                                                    # is sorted acc. to line number
-            prev = l-1
-        basicBlocks.append(list(self.prog[prev:]))
-        self.basicBlocks = basicBlocks
-        return self.basicBlocks
+        for leaderPair in pairwise(leaders):
+            self.basicBlocks.append(self.prog[leaderPair[0]-1:leaderPair[1]-1)
+
+# Auxiliary function
+def pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
