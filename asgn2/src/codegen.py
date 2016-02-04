6f575eaafb64f9db25c18d76003b7088d851764d
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-02-04 16:39:21 +0530

    Added basic block generation code, may be buggy

--- asgn2/src/codegen.py
+++ asgn2/src/codegen.py
@@ -10,4 +10,19 @@ class Codegen(object):
         self.basicBlocks = []
     # Method to compute basic blocks from the IR program
     def getBasicBlocks(self):
-        for instr in program:
+        leaders = list()
+
+        for instr in self.program:      #Yet to add for calling/return statements
+            if instr.isIfGoto() or instr.isGoto():
+                leaders.append(instr.Target)
+                leaders.append(instr.LineNo + 1)
+        leaders.sort()
+        basicBlocks = []
+        prev = 0
+        for l in leaders:
+            basicBlocks.append(list(self.prog[prev:l-2]))   #all this assuming program 
+                                                        # is sorted acc. to line number
+            prev = l-1
+        basicBlocks.append(list(self.prog[prev:]))
+        self.basicBlocks = basicBlocks
+        return self.basicBlocks
