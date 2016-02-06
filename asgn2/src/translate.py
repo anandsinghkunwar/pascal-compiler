f2e2714751381442f939ebbfd27b7b32296c8ca8
Aadi Yogakar <ayogakar@gmail.com>
2016-02-07 04:45:03 +0530

    binary operator assignment instruction completed

--- asgn2/src/translate.py
+++ asgn2/src/translate.py
@@ -10,23 +10,40 @@ def translateBlock(bb):
         globject.currInstr = instr
         string = ""
         if instr.isAssign:
-            if instr.Src2:  #assignment with binary operator
-                if instr.Src1.isInt():
-                    loc = bb.getReg()
-                    if not type(loc) == machine.Register:
-                        loc = machine.getEmptyRegister()
-                        if not loc:
-                            loc = machine.getOccupiedRegister()
+            if instr.Src2:  #assignment with binary operator    x = y op z
+                location = bb.getReg()
+                loc = location[0]
+                if not type(loc) == machine.Register:
+                    loc = machine.getEmptyRegister()
+                    if not loc:
+                        loc = machine.getOccupiedRegister()
+                if instr.Src1.isInt():  # y is integer
                     string +=  indent + "movl $" + instr.Src1.operand + ",%" + loc.name + "\n"
-                    op = getOperator(instr.op)
-                    if instr.Src2.isInt():
-                        string += indent + op + " $" + instr.Src2.operand + ",%" + loc.name + "\n"
-                    elif instr.Src2.reg:    #src2 exists in a register
-                        string += indent + op + " %" + instr.Src2.operand.reg.name + ",%" + loc.name + "\n"
-                    instr.Dest.operand.loadIntoRegister(loc.name)
-                    loc.addVar(instr.Dest.operand.name)
-                else:   #Src1 is variable
+                elif location[1]:   # y is variable and getReg returned y's register. so loc has y's value
                     pass
+                else:   # y is a variable and loc dosen't have y's value
+                    string += indent + "movl " + instr.Src1.operand.name + ",%" + loc.name + "\n"
+                op = getOperator(instr.op)
+                if instr.Src2.isInt():
+                    string += indent + op + " $" + instr.Src2.operand + ",%" + loc.name + "\n"
+                elif instr.Src2.reg:    #z exists in a register
+                    flag = False
+                    if instr.Src2.reg.name != "ecx" and (op == "sal" or op == "sar"):
+                        string += indent + "xchgl %ecx,%" + instr.Src2.operand.reg.name + "\n"
+                        flag = True
+                    string += indent + op + " %" + instr.Src2.operand.reg.name + ",%" + loc.name + "\n"
+                    if flag:
+                        string += indent + "xchgl %ecx,%" + instr.Src2.operand.reg.name + "\n"
+                else:   #z dosen't exist in a register
+                    flag = False
+                    if instr.Src2.reg.name != "ecx" and (op == "sal" or op == "sar"):
+                        string += indent + "xchgl %ecx," + instr.Src2.operand.name + "\n"
+                        flag = True
+                    string += indent + op + " " + instr.Src2.operand.name + ",%" + loc.name + "\n"
+                    if flag:
+                        string += indent + "xchgl %ecx," + instr.Src2.operand.name + "\n"
+                instr.Dest.operand.loadIntoRegister(loc.name)
+                loc.addVar(instr.Dest.operand.name)
             elif instr.Op:  #assignment wirh unary operator
                 pass
             else:           #basic assignment
@@ -52,6 +69,6 @@ def getOperator(op):
     elif op == 3:   #div consumes too many registers, will handle later
         pass
     elif op == 9:
-        return "shl"
+        return "sal"
     elif op == 10:
         return "sar"
