f51ccbb93d4db5dadaf88992632363954256f3f8
Saransh Srivastava <ssaransh@cse.iitk.ac.in>
2016-04-02 04:17:34 +0530

    Module for IR code generation

---
+++ asgn4/src/ircodegen.py
@@ -0,0 +1,11 @@
+# Module for IR code generation.
+import symbol_table as ST
+
+# Dummy class to enable parser to use attributes
+class Node(object):
+    def __init__(self):
+        self.value = None
+        self.code = ''
+        self.place = None
+        self.items = []
+        self.type = None
