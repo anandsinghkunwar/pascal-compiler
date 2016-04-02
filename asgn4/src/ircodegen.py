d88f6c83282d01e2bbbfebfbfd22cef72f4e46c9
Aadi Yogakar <ayogakar@gmail.com>
2016-04-03 03:48:25 +0530

    Operand class added

--- asgn4/src/ircodegen.py
+++ asgn4/src/ircodegen.py
@@ -11,3 +11,48 @@ class Node(object):
         self.type = None
         self.arrayBeginList = []
         self.arrayEndList = []
+
+# Class to handle instruction operands
+class Operand(object):
+    # Operand Types
+    INT, STRING, BOOL, INTVAR, STRINGVAR, BOOLVAR, ARRAY, ARRAYELEMENT = range(7)
+
+    def __init__(self, varObj):
+        if isinstance(varObj, ST.SymTabEntry):
+            self.operand = varObj.name
+            if varObj.type.type == ST.Type.INT:
+                self.operandType = Operand.INTVAR
+            elif varObj.type.type == ST.Type.STRING:
+                self.operandType = Operand.STRINGVAR
+            elif varObj.type.type == ST.Type.BOOL:
+                self.operandType = Operand.BOOLVAR
+            else:
+                self.operandType = Operand.ARRAY
+        elif type(varObj) is int:
+            self.operand = varObj
+            self.operandType = Operand.INT
+        elif type(varObj) is str:
+            self.operand = varObj
+            self.operandType = Operand.STRING
+        elif type(varObj) is bool:
+            self.operand = varObj
+            self.operandType = Operand.BOOL
+        else:   #ARRAYELEMENT
+            # TODO
+
+    def isInt(self):
+        return self.operandType == Operand.INT
+    def isString(self):
+        return self.operandType == Operand.STRING
+    def isBool(self):
+        return self.operandType == Operand.BOOL
+    def isIntVar(self):
+        return self.operandType == Operand.INTVAR
+    def isStringVar(self):
+        return self.operandType == Operand.STRINGVAR
+    def isBoolVar(self):
+        return self.operandType == Operand.BOOLVAR
+    def isArray(self):
+        return self.operandType == Operand.ARRAY
+    def isArrayElement(self):
+        return self.operandType == Operand.ARRAYELEMENT
