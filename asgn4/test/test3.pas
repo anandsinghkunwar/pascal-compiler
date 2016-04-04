5302eb0dc7f5d52266f474f33c9727f8ed631893
Saransh Srivastava <ssaransh@cse.iitk.ac.in>
2016-04-05 04:22:07 +0530

    Short circuiting test case

--- asgn4/test/test3.pas
+++ asgn4/test/test3.pas
@@ -5,27 +5,16 @@ var
     x : boolean;
 
 Begin
-    // For Loop Construct
-    for a := 10  to 20 do
-    begin
-        writeln('value of a: ', a);
-        b := 'a'
-    end;
-
     // While Loop Construct
-    a := 5;
+    a := 3;
     while a < 6 do
     begin
         writeln (a);
         a := a + 1
     end;
 
-    // Repeat Loop Construct
-    x := 1;
-    repeat
-    begin
-        x := x + 1;
-    end;
-    until x = 10;
-
+    if true or false then
+       begin
+          writeln(a)
+       end;
 End.
