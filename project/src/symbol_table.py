ea714084f5d8d976732e4a7a3da33a34b4cab425
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-04-12 04:25:10 +0530

    Type class corrected with helper functions

--- project/src/symbol_table.py
+++ project/src/symbol_table.py
@@ -19,17 +19,14 @@ class Type(object):
     def __eq__(self, other):
         if other is None:
             return False
-        if self.isConst and not other.isConst:
-            return self.type == other.baseType
-        return self.name == other.name
+        # TODO FIXME Why was this here?
+        # if self.isConst and not other.isConst:
+        #     return self.type == other.baseType
+        return self.getDeepestType() == other.getDeepestType()
 
     # Enumeration for types
     # TODO: Pointers
     INT, BOOL, CHAR, STRING, ARRAY, FUNCTION, PROCEDURE, KEYWORD, TYPE, PROGRAM = range(10)
-
-    # TODO: Is this redundant...
-    def isBuiltin(self):
-        return self.baseType == None
 
     def getDeepestType(self):
         if self.baseType is None:
@@ -39,7 +36,7 @@ class Type(object):
 
 # Class to implement a symbol table entry.
 class SymTabEntry(object):
-    def __init__(self, name, type, mySymTab, nextSymTab=None, isConst=False, isParameter=False, isTemp=False):
+    def __init__(self, name, type, mySymTab, nextSymTab=None, isConst=False, isParameter=False, isTemp=False, isOverridable=False):
         self.name = name
         self.type = type
         self.mySymTab = mySymTab
@@ -47,6 +44,7 @@ class SymTabEntry(object):
         self.isConst = isConst
         self.isParameter = isParameter
         self.isTemp = isTemp
+        self.isOverridable = isOverridable
 
     def scope(self):
         return self.mySymTab.scope
@@ -54,32 +52,32 @@ class SymTabEntry(object):
     def printEntry(self):
         print "\t" + self.name + "\t|\t" + self.type.name
 
-    # Type checking methods
+    # TODO Test Type checking methods
     def isInt(self):
-        return self.type.type == Type.INT
+        return self.type.getDeepestType() == 'integer'
     def isBool(self):
-        return self.type.type == Type.BOOL
+        return self.type.getDeepestType() == 'boolean'
     def isChar(self):
-        return self.type.type == Type.CHAR
+        return self.type.getDeepestType() == 'char'
     def isString(self):
-        return self.type.type == Type.STRING
+        return self.type.getDeepestType() == 'string'
     def isArray(self):
-        return self.type.type == Type.ARRAY
+        return self.type.getDeepestType() == 'array'
     def isFunction(self):
-        return self.type.type == Type.FUNCTION
+        return self.type.getDeepestType() == 'function'
     def isProcedure(self):
-        return self.type.type == Type.PROCEDURE
+        return self.type.getDeepestType() == 'procedure'
     def isKeyword(self):
-        return self.type.type == Type.KEYWORD
+        return self.type.getDeepestType() == 'keyword'
     def isType(self):
         return self.type.type == Type.TYPE
     def isProgram(self):
-        return self.type.type == Type.PROGRAM
+        return self.type.getDeepestType() == 'program'
     def isConstant(self):
         return self.isConst
     # Predefined = Overridable
     def isPredefined(self):
-        return self.type.isBuiltin()
+        return self.isOverridable
 
 class SymTab(object):
     def __init__(self, previousTable):
@@ -89,22 +87,15 @@ class SymTab(object):
         self.childrenTables = []
         SymTab.nextScope += 1
 
-        # Add built in types
-        self.addVar('integer', Type('integer', Type.TYPE))
-        self.addVar('boolean', Type('boolean', Type.TYPE))
-        self.addVar('char', Type('char', Type.TYPE))
-        self.addVar('array', Type('array', Type.TYPE))
-        self.addVar('string', Type('string', Type.TYPE))
-
     # Class variables for allocation
     nextScope = 0
 
     def addTable(self, symTab):
         self.childrenTables.append(symTab)
 
-    def addVar(self, varName, varType, isParameter=False, isTemp=False, isConst=False):
+    def addVar(self, varName, varType, isParameter=False, isTemp=False, isConst=False, isOverridable=False):
         if not self.entryExists(varName):
-            self.entries[varName] = SymTabEntry(varName + "." + str(self.scope), varType, self, isParameter=isParameter, isTemp=isTemp, isConst=isConst)
+            self.entries[varName] = SymTabEntry(varName + "." + str(self.scope), varType, self, isParameter=isParameter, isTemp=isTemp, isConst=isConst, isOverridable=isOverridable)
             return self.entries[varName]
         else:
             # TODO: Handle error?
@@ -146,6 +137,13 @@ class SymTab(object):
 # The root symbol table in the tree structure.
 rootSymTab = SymTab(None)
 
+# Add built in types
+rootSymTab.addVar('integer', Type('integer', Type.TYPE), isOverridable=True)
+rootSymTab.addVar('boolean', Type('boolean', Type.TYPE), isOverridable=True)
+rootSymTab.addVar('char', Type('char', Type.TYPE), isOverridable=True)
+rootSymTab.addVar('array', Type('array', Type.TYPE), isOverridable=True)
+rootSymTab.addVar('string', Type('string', Type.TYPE), isOverridable=True)
+
 # The current symbol table that is being used.
 currSymTab = rootSymTab
 
@@ -154,7 +152,7 @@ def lookup(identifier):
     tempSymTab = currSymTab
 
     while tempSymTab != None:
-        if identifier in tempSymTab.entries.keys():
+        tempSymTab.entryExists(identifier):
             return tempSymTab.entries[identifier]   # Found identifier
         tempSymTab = tempSymTab.previousTable
     return None
