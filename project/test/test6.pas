be148fcc1431af0ec857634b39ce860dda250829
Aadi Yogakar <ayogakar@gmail.com>
2016-04-12 23:36:15 +0530

    test for char parameter printing. Not working

--- project/test/test6.pas
+++ project/test/test6.pas
@@ -1,8 +1,9 @@
 program p;
-function foo(a,b,c : integer) : integer;
+function foo(a,b,c : integer; d : char) : integer;
 begin
    //a := 5;
     writeln(a);
+    writeln(c);
    foo := 7;
 end;
 
@@ -10,6 +11,6 @@ var
    d : integer;
 
 begin
-   d := foo(1,2,3);
+   d := foo(1,2,3,'f');
    writeln(d)
 end.
