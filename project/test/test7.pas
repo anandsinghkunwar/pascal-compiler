19e698c7c619fb7e226583e64e5f3da9781f20ae
Aadi Yogakar <ayogakar@gmail.com>
2016-04-14 02:41:20 +0530

    test for scanf and recursion

--- project/test/test7.pas
+++ project/test/test7.pas
@@ -1,28 +1,18 @@
 program p;
 function foo(a : integer) : integer;
-var
-    b : integer;
-    c : integer;
 begin
-    b := a - 1;
-    writeln(b);
     if a = 1 then
         foo := 1
     else 
     begin
-        c := foo(b);
-        writeln('now');
-        writeln(c);
-        b := a * c;
-        writeln('the value is');
-        writeln(b);
-        foo := b
+        foo := a * foo(a - 1)
     end;
 end;
 
 var
     d : integer;
 begin
-    d := foo(4);
+    read(d);
+    d := foo(d);
     writeln(d)
 end.
