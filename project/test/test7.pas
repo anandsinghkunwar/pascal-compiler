e288856c513ac6620bfe99364ba8b939ac452d28
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-04-13 14:45:29 +0530

    Test 7

--- project/test/test7.pas
+++ project/test/test7.pas
@@ -1,18 +1,12 @@
 program p;
-function foo(a : integer; d : char) : integer;
+function foo(a : integer) : integer;
 begin
-    while a < 6 do
-    begin
-        writeln(d);
-    end;
     foo := 6;
 end;
 
 var
-    a : integer;
     d : integer;
+
 begin
-    a := 1;
-    d := foo(1,'f');
-    writeln(d)
+        writeln('dsfds')
 end.
